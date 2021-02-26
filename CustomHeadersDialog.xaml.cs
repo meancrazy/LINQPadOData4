d340c07558e292b2bd93fe1f987e2a9b4d8b74fc
Dilshod Komilov <komilovd@silkroadprofessionals.com>
2021-02-26 10:11:26 +0500

    add custom headers

---
+++ CustomHeadersDialog.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace OData4.LINQPadDriver
+{
+	/// <summary>
+	/// Interaction logic for CustomHeadersDialog.xaml
+	/// </summary>
+	public partial class CustomHeadersDialog : Window
+	{
+		public List<CustomHeader> CustomHeaders { get; set; } = new List<CustomHeader>();
+		public CustomHeadersDialog()
+		{
+			InitializeComponent();
+			DataContext = this;
+		}
+
+		private void OK_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void Cancel_Click(object sender, RoutedEventArgs e)
+		{
+			CustomHeaders.Clear();
+			this.Close();
+		}
+	}
+
+	public class CustomHeader
+	{
+		public string Name { get; set; }
+
+		public string Value { get; set; }
+	}
+}
